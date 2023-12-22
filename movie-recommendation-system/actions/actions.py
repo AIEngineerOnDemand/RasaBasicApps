600eeee320030d8b39930516884df5fe954a27cb
AIEngineerOnDemand <giopantana@gmail.com>
2023-12-22 00:46:24 -0500

    Add movie recommendations API integration

--- movie-recommendation-system/actions/actions.py
+++ movie-recommendation-system/actions/actions.py
@@ -3,6 +3,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+import requests
 
 class ActionRecommendMovie(Action):
 
@@ -14,43 +15,50 @@ class ActionRecommendMovie(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
         genre = next(tracker.get_latest_entity_values("genre"), None)
-
-        if genre == "comedy":
-            recommeded_movie = ["The Hangover", "Knocked Up", "Superbad", "Step Brothers",
-                                "Bridesmaids", "Dumb and Dumber", "Anchorman", "Old School", 
-                                "Wedding Crashers", "Talladega Nights", "Mean Girls", "Pitch Perfect",
-                                "Zoolander", "Napoleon Dynamite", "Anchorman 2: The Legend Continues",
-                                "The 40-Year-Old Virgin", "Elf", "Airplane!", "Shaun of the Dead"]
-        elif genre == "action":
-            recommeded_movie = ["The Dark Knight", "The Lord of the Rings: The Return of the King", 
-                                "Inception", "The Lord of the Rings: The Fellowship of the Ring",
-                                "The Matrix", "Gladiator", "Braveheart", "Saving Private Ryan",
-                                "The Departed", "The Lord of the Rings: The Two Towers", "Mad Max: Fury Road",
-                                "Die Hard", "The Avengers", "Mission: Impossible - Fallout",
-                                "John Wick", "The Bourne Identity", "The Terminator", "Kill Bill"]
-        elif genre == "horror":   
-            recommeded_movie = ["The Exorcist", "The Shining", "The Conjuring", "The Cabin in the Woods",
-                                "The Babadook", "The Descent", "The Witch", "The Orphanage", 
-                                "The Others", "The Ring", "Get Out", "A Quiet Place", "Hereditary",
-                                "Psycho", "Halloween", "Nightmare on Elm Street", "The Texas Chainsaw Massacre"]
-        elif genre == "romantic":
-            recommeded_movie = ["Titanic", "The Notebook", "The Fault in Our Stars", "A Walk to Remember",
-                                "The Vow", "Dear John", "The Lucky One", "The Last Song", 
-                                "The Best of Me", "Safe Haven", "La La Land", "Before Sunrise", "Eternal Sunshine of the Spotless Mind",
-                                "Romeo + Juliet", "500 Days of Summer", "The Princess Bride", "Crazy Rich Asians"]
-        elif genre == "drama": 
-            recommeded_movie = ["The Shawshank Redemption", "The Godfather", "The Godfather: Part II",
-                                "Pulp Fiction", "Fight Club", "Goodfellas", "The Social Network",
-                                "The Green Mile", "The Prestige", "American Beauty",
-                                "Schindler's List", "The Great Gatsby", "The Revenant", "The King's Speech"]
-        else :
-            recommeded_movie =[]
-            
-        if recommeded_movie:
-            response = f"Here are some {genre} movies you might like: " + ", ".join(recommeded_movie)
-        else:       
+        
+        if not genre:
             response = "Sorry, I do not have any movie recommendations for that genre."
+            disparcher.utter_message(text=response)
+            return []
         
-        dispatcher.utter_message(text=response)
+        # Make API request to fetch movie recommendations based on genre
+        api_key = "your_api_key"
         
+        # Fetch genre ID from TMDb API
+        genre_id = self.get_genre_id(genre, api_key)
+        
+        if genre_id:
+            url = f'https://api.themoviedb.org/3/discover/movie?api_key={api_key}&with_genres={genre_id}'
+            response = requests.get(url)
+            
+            if response.status_code == 200:
+                movies = response.json().get("results",[])
+                if movies:
+                    response_text = f"Here are some {genre} movies you might like - "
+                    for movie in movies[:5]:
+                        response_text += f"{movie['title']} ({movie['release_date'][:4]}), "
+                    response_text = response_text[:-2]
+                else:
+                    response_text = "I'm sorry, I couldn't find any recommendations for that genre."
+            else:
+                response_text = "Oops! Something went wrong while fetching recommendations. Please try again later."
+        else:
+            response_text = "I'm sorry, I couldn't find that genre in my database."    
+            
+        dispatcher.utter_message(text=response_text)
+                        
         return []
+    
+    @staticmethod
+    def get_genre_id(genre, api_key):
+        url = f"https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}&language=en-US"
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            genres = response.json().get("genres",[])
+            for g in genres:
+                if g["name"].lower() == genre.lower():
+                    return g["id"]
+        return None
+
+
